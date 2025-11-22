3486d8bc6e44f11bb38069b2fb63a8c3d8a489f6
Andrew Lomaksan Manuel Tampubolon <andrewmanuelt@gmail.com>
2025-11-22 18:28:27 +0700

    major update

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.docstore import InMemoryDocstore
 from langchain_huggingface import HuggingFaceEmbeddings
 
-LLM_REPOSITORY='' # Isi hanya dengan Ollama. Ganti dengan repository LLM lainnya
+LLM_REPOSITORY='google/gemma-3-1b-it:Q8_0' # Isi hanya dengan Ollama. Ganti dengan repository LLM lainnya
 
 GEMINI_API_KEY='' # Isi API KEY untuk penggunaan dengan Gemini
 GEMINI_MODEL='gemini-2.5-flash-lite' # pilihan: gemini-2.5-flash, gemma-3-27b-it
@@ -27,7 +27,7 @@ VECTORSTORE_INDEX='lpdp'
 def chat_stream(prompt):
     for char in prompt:
         yield char 
-        time.sleep(0.02)
+        time.sleep(0.015)
 
 def save_feedback(index):
     st.session_state.history[index]['feedback'] = st.session_state[f'feedback_{index}']
@@ -59,8 +59,11 @@ def preprocess_context(ctx_filename, new_ctx_filename):
         sys.exit(1)
         
     with open(ctx_filename, 'r', encoding='utf-8') as f:
-        context = [txt.strip() for txt in f.readlines() if txt not in ['\n', '\x0c', '']]
-        context = [txt for txt in context if txt != '']
+        context = [txt for txt in f.readlines()]
+        context = [remove_unwanted_char(txt) for txt in context]
+        context = [txt for txt in context if txt != '' or len(txt) > 3] 
+        
+        print(context)
     
     context = ' '.join(context).replace('..', '.')
     with open(new_ctx_filename, 'a', encoding='utf-8') as f:
@@ -69,9 +72,15 @@ def preprocess_context(ctx_filename, new_ctx_filename):
         
     return context     
 
+def remove_unwanted_char(text: str):
+    text = text.replace('\n', '').replace('\x0c', '')
+    text = re.sub(r'^[\d\w]{0,2}+[\.\)]', '', text)
+    text = re.sub(r'[\?\:\✓\*\●\;]', '', text)
+    return text.strip()
+
 def prepare_context():
     create_context(src_filename='public_policy_file_1751536393.pdf', 
-                   dst_filename='lpdp-selection-page.pdf',
+                   dst_filename='lpdp-selected-page.pdf',
                    ctx_filename='context.txt')
 
     ctx = preprocess_context('context.txt', 'new_context.txt')
@@ -102,7 +111,7 @@ def main():
         with st.chat_message("assistant"):
             context = get_context(prompt, VECTORSTORE_PATH, VECTORSTORE_INDEX)
             
-            response = generate_with_gemini(prompt, context) # generate(prompt, context)
+            response = generate(prompt, context) # generate_with_gemini(prompt, context)
             response = st.write_stream(chat_stream(response))
             
             st.feedback(
@@ -114,14 +123,92 @@ def main():
         st.session_state.history.append({"role": "assistant", "content": response})
             
 def generate(question, ctx):
-    question = prompt(question, ctx)    
+    question = prompt_cot(question, ctx)    
         
     print('\n', question, '\n')
         
     return request(question)
+
+def prompt_cove(question, ctx):
+    return f'''\nJawab pertanyaan dengan memberikan statement awal terhadap pertanyaan yang diajukan.
+    Selelah statement dibuat, lakukan verifikasi terhadap pernyataan tersebut. 
+    Gunakan [konteks] sebagai informasi tambahan untuk memperdalam pengetahuan dalam menjawab pertanyaan.
+    Berikut adalah contoh dari proses verifikasi tersebut:
+
+    [pertanyaan]: 
+    Apakah Cristiano Ronaldo pemenang Euro 2004 ? 
+
+    [statement]: 
+    Cristiano Ronaldo adalah pemain muda Portugal yang menjuarai Final Euro 2004
+
+    [verifikasi]:
+    1. Ronaldo adalah benar pemain Portugal yang bermain pada Euro 2004
+    2. Pada Euro 2004 Portugal adalah salah satu negara yang menjadi finalis
+    3. Menemukan informasi bahwa pada Euro 2004 Portugal kalah dari Yunani
+    4. Karena Portugal kalah dari Yunani maka juara Euro 2004 adalah Portugal, bukan Yunani
+    5. Statement sebelumnya adalah benar karena Portugal kalah 1-0. Untuk itu Cristiano Ronaldo benar pemain muda Portugal, namun tidak menjuarai Final Euro 2004
+
+    [jawaban]: Maka berdasarkan hasil verifikasi dapat dinyatakan bahwa Cristiano Ronaldo tidak menjuarai Euro 2004
+    bersama Portugal. Tetapi benar bahwa Cristiano Ronaldo pemain muda Portugal pada kejuaran tersebut
+
+    Jawablah pertanyaan berikut ini!
     
-def prompt(question, ctx):
-    return f'''\nJawablah pertanyaan berikut dan gunakan konteks berikut sebagai sumber informasi ! \npertanyaan: {question} \nkonteks: {ctx} \nJawaban: \n'''
+    [pertanyaan]: {question}
+    [konteks]: {ctx}
+    [jawaban]:'''
+
+def prompt_cot(question, ctx):
+    return f'''\nJawablah pertanyaan berikut dengan melakukan analisis secara sekuensial terhadap hal yang ditanyakan!
+    Gunakan [konteks] sebagai informasi tambahan untuk memperdalam pengetahuan dalam menjawab pertanyaan.
+    Berikut adalah contoh dari pertanyaan dan proses menjawab:
+
+    [pertanyaan]: 
+    Apakah Cristiano Ronaldo pemenang Euro 2004 ?
+
+    [analisis]:
+    1. Cristiano Ronaldo adalah pemain Portugal yang berposisi sebagai winger 
+    2. Dia bermain bersama pemain top seperti Quaresma dan Deco 
+    3. Portugal merupakan tuan rumah Euro 2004
+    4. Pemenang Euro 2004 adalah tim yang menang pada partai final
+    5. Portugal dikalahkan oleh Yunani dalam partai final dengan skor 1-0
+    6. Gol tunggal dibuat oleh Angelos Charisteas
+
+    [jawaban]: Cristiano Ronaldo bukanlah juara Euro 2024, melainkan Yunani yang menang dengan skor 1-0 melalui gol Angelos Charisteas
+    
+    Jawablah pertanyaan berikut ini!
+    
+    [pertanyaan]: {question}
+    [konteks]: {ctx}
+    [jawaban]:'''
+    
+    
+def prompt_react(question, ctx):
+    return f'''\nSelesaikan tugas menjawab pertanyaan dengan langkah-langkah yang dimasukkan: Pemikiran, Tindakan, dan Observasi. Langkah tersebut dapat dilakukan beberapa kali hingga jawaban akhir ditemukan. Pemikiran dapat merenungkan situasi saat ini, dan Tindakan dapat terdiri dari tiga jenis:  
+        (1) Search[entitas], yang mencari Wikipedia untuk entitas yang sesuai dan mengembalikan paragraf pertama jika ada. Jika tidak ada, maka akan mengembalikan beberapa entitas serupa untuk dicari.  
+        (2) Search[kata kunci], yang mengembalikan kalimat berikutnya yang mengandung kata kunci dalam bagian saat ini.  
+        (3) Complete[jawaban], yang mengembalikan jawaban dan menyelesaikan tugas. 
+        Konteks bersifat opsional. Gunakan konteks sebagai informasi hanya apabila relevan.
+        
+        Teks berikut adalah contoh langkah-langkah dalam menjawab pertanyaan:
+
+        Pemikiran 1: Saya perlu mengetahui peran Cristiano Ronaldo dan kondisi tim Portugal pada Euro 2004.
+        Tindakan 1: Search[informasi Ronaldo dan skuad Portugal Euro 2004]
+        Observasi 1: Cristiano Ronaldo adalah winger Portugal yang bermain bersama pemain top seperti Quaresma dan Deco, dan Portugal menjadi tuan rumah Euro 2004.
+
+        Pemikiran 2: Untuk menemukan pemenang Euro 2004, saya harus mengetahui hasil pertandingan final.
+        Tindakan 2: Search[hasil final Euro 2004]
+        Observasi 2: Portugal kalah dari Yunani di final Euro 2004 dengan skor 1–0.
+
+        Pemikiran 3: Saya perlu mengetahui siapa pencetak gol dalam pertandingan final tersebut.
+        Tindakan 3: Search[pencetak gol final Euro 2004]
+        Observasi 3: Gol tunggal pertandingan final dicetak oleh Angelos Charisteas.
+        
+        Jawablah pertanyaan berikut ini dengan mengganti teks `<!jawaban>` dengan jawaban akhir!
+
+        [pertanyaan]: {question}
+        [konteks]: {ctx}
+        
+        <!jawaban>'''
     
 def request(question):
     body = {
@@ -129,14 +216,14 @@ def request(question):
         'messages': [
             { 
                 'role': 'system', 
-                'content': 'You are a helpful assistant'
+                'content': 'You are a helpful assistant.'
             },
             { 
                 'role': 'user', 
                 'content': question,
             }
         ],
-        'max_tokens': 500
+        'max_tokens': 700
     }
     
     response = requests.post(
@@ -148,9 +235,11 @@ def request(question):
     )
     response = response.json()
     response = response['choices'][0]['message']['content'].replace('*', '').replace('\n', ' ')
+    response = re.sub(r'\s+', ' ', response)
+    response = re.sub(r'(?<!^)(\d+\.)', r'\n\1', response)
+    response = response.replace('[pertanyaan]:', '\n').replace('[statement]:', '\n').replace('[verifikasi]:', '').replace('[jawaban]:', '')
+    return response
     
-    return re.sub(r'\s+', ' ', response) 
-
 def faiss_store(embedding):
     index = faiss.IndexHNSWFlat(len(embedding.embed_query('helo')), 32)
     return FAISS(
@@ -175,7 +264,7 @@ def get_context(query, path, index):
     embedding = HuggingFaceEmbeddings(model_name='BAAI/bge-m3', encode_kwargs={"normalize_embeddings": True})
     
     vecstore = faiss_client(path, index, embedding)
-    results = vecstore.similarity_search_with_relevance_scores(query, 5)
+    results = vecstore.similarity_search_with_relevance_scores(query, 10)
 
     contexts = []
     for context in results:
@@ -203,7 +292,7 @@ def store_context(ctx: str, path, index):
 def generate_with_gemini(question, ctx):
     client = genai.Client(api_key=GEMINI_API_KEY)
     
-    question = prompt(question, ctx)    
+    question = prompt_cot(question, ctx)    
     
     print('\n', question, '\n')
     
